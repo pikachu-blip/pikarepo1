76838bf108a070a902c90e12a8f53eac2ed75f81
pikachu-blip <kumarkrishh42@gmail.com>
2025-04-08 02:50:02 +0530

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+ENVIRONMENT = os.getenv("ENVIRONMENT", "DEV")
+
+if ENVIRONMENT==dev:
+  env_file =".env.DEV"
+else:
+  env_file =".env.PROD"
+
+# Load the chosen .env file
+load_dotenv(env_file,override=True)
+
+print(f"PROJECT_ID: {os.getenv('PROJECT_ID')}") 
+print(f"PROJECT_NO: {os.getenv('PROJECT_NUMBER')}") 
+
+
