aa2d7f51a4e7583b985a9ca7e9e59d38f62ae7c2
pikachu-blip <kumarkrishh42@gmail.com>
2025-04-08 15:56:08 +0530

    Update abc.py

--- testerkk/abc.py
+++ testerkk/abc.py
@@ -1,17 +1,23 @@
 import os
 from dotenv import load_dotenv
 
-ENVIRONMENT = os.getenv("ENVIRONMENT")
-print(ENVIRONMENT)
+# ENVIRONMENT = os.getenv("ENVIRONMENT")
+# print(ENVIRONMENT)
 
-if ENVIRONMENT=="dev":
-  env_file =".env.DEV"
-else:
-  env_file =".env.PROD"
 
-# Load the chosen .env file
-load_dotenv(env_file,override=True)
+# Get the environment selected in GitHub Actions
+GITHUB_ENVIRONMENT = os.getenv("GITHUB_ENVIRONMENT")  # Default to dev
 
-print(f"PROJECT_ID: {os.getenv('PROJECT_ID')}") 
-print(f"PROJECT_NO: {os.getenv('PROJECT_NUMBER')}") 
+print(f"Running in GitHub Environment: {GITHUB_ENVIRONMENT}")
+
+# if ENVIRONMENT=="dev":
+#   env_file =".env.DEV"
+# else:
+#   env_file =".env.PROD"
+
+# # Load the chosen .env file
+# load_dotenv(env_file,override=True)
+
+# print(f"PROJECT_ID: {os.getenv('PROJECT_ID')}") 
+# print(f"PROJECT_NO: {os.getenv('PROJECT_NUMBER')}") 
 
